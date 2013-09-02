786adca68e048397a440740dba2afa27859004cc
Sean Gee <seangee@gmail.com>
2013-09-02 11:29:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TasteSpottingClone::Application.config.secret_token = 'e997047792af18da55ae7e861d013daaf0c8a8aba50ea2df4c26af39263fe385671bebd7d4e74a68f6fee1f5de06a39fe9c4c50a3f84bd8a708c780936c0f295'
